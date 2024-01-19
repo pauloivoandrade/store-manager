971124b40962efefa4fe7f46ae93b33ce38aba14
pauloivoandrade <pauloivoasantos@gmail.com>
2024-01-19 09:13:19 -0300

    Sales Mock

--- backend/src/controllers/index.js
+++ backend/src/controllers/index.js
@@ -1,5 +1,7 @@
 const productController = require('./product.Controller');
+const salesController = require('./sales.Controller');
 
 module.exports = {
   productController,
+  salesController,
 };
---
+++ backend/tests/unit/mocks/sales.mock.js
@@ -0,0 +1,22 @@
+const allSalesMock = [
+  {
+    id: 1,
+    date: '2024-01-19T01:25:53.000Z',
+  },
+  {
+    id: 2,
+    date: '2024-01-19T01:25:53.000Z',
+  },
+];
+
+const salesByIdMock = [
+  {
+    id: 2,
+    date: '2024-01-19T01:25:53.000Z',
+  },
+];
+
+module.exports = {
+  allSalesMock,
+  salesByIdMock,
+};
